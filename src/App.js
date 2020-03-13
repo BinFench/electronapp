19d740f5760adcaf098132facd99bf7a3c0700c3
BinFench <bcfinch@edu.uwaterloo.ca>
2020-03-13 13:02:14 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+// const electron = window.require('electron');
+// const fs = electron.remote.require('fs');
+// const ipcRenderer  = electron.ipcRenderer;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
